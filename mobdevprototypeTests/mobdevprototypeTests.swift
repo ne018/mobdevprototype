3763e946517dd7af16a3f25a8d2c124c2f64862e
MacBook Pro <drey01814@gmail.com>
2018-05-28 17:11:55 +0800

    Read me update.

--- mobdevprototypeTests/mobdevprototypeTests.swift
+++ mobdevprototypeTests/mobdevprototypeTests.swift
@@ -11,26 +11,15 @@ import XCTest
 
 class mobdevprototypeTests: XCTestCase {
     
+    var carListController : CarListController!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        carListController = CarListController(collectionViewLayout: UICollectionViewFlowLayout())
+        let navigationController = UINavigationController.init(rootViewController: carListController)
+        XCTAssertNotNil(carListController.view)
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+
 }
